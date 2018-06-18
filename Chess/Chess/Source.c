e6f76fd6ae9822efcdc5cbc3f3847c4c2b06e4c3
dmusial98 <dmusial98@gmail.com>
2018-06-18 05:14:05 +0200

    Small changes

--- Chess/Chess/Source.c
+++ Chess/Chess/Source.c
@@ -324,7 +324,7 @@ Bool Does_Bishop_threaten_King(Board_struct** Board, Pieces colour_of_King, Piec
 				for (int a = 0; a < 8; a++) {
 					for (int b = 0; b < 8; b++) {
 						if (Board[a][b].Piece_on_square == colour_of_King) {
-							if (a - i == b - j) {
+							if (abs(a - i) == abs(b - j)) {
 								if (Main_funct_is_it_empty_Bishop(Board, b, a, i, j)) return True;
 							}
 						}
@@ -334,6 +334,45 @@ Bool Does_Bishop_threaten_King(Board_struct** Board, Pieces colour_of_King, Piec
 		}
 	}
 	return False;
+}
+
+Bool Does_Knight_threaten_King(Board_struct** Board, Pieces colour_of_King, Pieces colour_of_Knight) {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (Board[i][j].Piece_on_square == colour_of_Knight) {
+				for (int a = 0; a < 8; a++) {
+					for (int b = 0; b < 8; b++) {
+						if (Board[a][b].Piece_on_square == colour_of_King) {
+							if (abs(a - i) == 2 && abs(b - j) == 1) return True;
+							else if (abs(a - i) == 1 && abs(b - j) == 2) return True;
+						}
+					}
+				}
+			}
+		}
+	}
+	return False;
+}
+
+Bool Does_Queen_threaten_King(Board_struct** Board, Pieces colour_of_King, Pieces colour_of_Queen) {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (Board[i][j].Piece_on_square == colour_of_Queen) {
+				for (int a = 0; a < 8; a++) {
+					for (int b = 0; b < 8; b++) {
+						if (Board[a][b].Piece_on_square == colour_of_King) {
+							if (a == i || b == j) {
+								if (Main_funct_is_it_empty_Rook(Board, b, a, i, j)) return True;
+							}
+							else if (abs(a - i) == abs(b - j)) {
+								if (Main_funct_is_it_empty_Bishop(Board, b, a, i, j)) return True;
+							}
+						}
+					}
+				}
+			}
+		}
+	} return False;
 }
 
 void display_Piece(Board_struct** Board, int i, int j){
